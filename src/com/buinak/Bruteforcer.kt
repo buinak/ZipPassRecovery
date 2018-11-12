ace43ac031470bde96ccb6504e5a05170d719321
buinak <fforeseer@gmail.com>
2018-11-12 16:46:46 +0100

    Some fixes.

--- src/com/buinak/Bruteforcer.kt
+++ src/com/buinak/Bruteforcer.kt
@@ -11,7 +11,8 @@ class Bruteforcer(
     val path: String,
     private val characters: List<Char>,
     val depth: Int = 1,
-    private val countIterations: Boolean = false
+    private val countIterations: Boolean = false,
+    private val printTried: Boolean = false
 ) {
 
     private var finished = false
@@ -20,9 +21,12 @@ class Bruteforcer(
 
     fun start() {
 
-        println("Creating a list of possible passwords for $depth depth..")
+        println("Creating a list of possible passwords for depth = $depth..")
         val passwords = getAllPossiblePasswords(depth)
         println("Maximum amount of iterations = ${passwords.size}")
+        val secondsMax = passwords.size / 5000
+        println("Assuming the speed of 5000 iterations per second, ")
+        println("the worst case scenario will take $secondsMax seconds, or ${secondsMax / 3600} hours.")
 
         val cpuCount = Runtime.getRuntime().availableProcessors()
         println("Allocating $cpuCount threads..")
@@ -33,29 +37,57 @@ class Bruteforcer(
             } else {
                 (subSize * (i - 1)) until passwords.size
             }
-
-            Completable.fromAction {
-                for (index in subRange) {
-                    iteration.getAndIncrement()
-                    if (Verifier.verify(path, passwords[index])) {
-                        finished = true
-                        println("Password to the zip archive at $path === ${passwords[index]}")
-                        Runtime.getRuntime().exit(0)
-                    }
-                }
-            }   .subscribeOn(Schedulers.computation())
-                .subscribe {}
+            when (printTried){
+                true -> startSearchInRangeWithPrinting(subRange, passwords)
+                false -> startSearchInRange(subRange, passwords)
+            }
         }
 
         //print the number of iterations on the main thread
         while (true){
             val actSec = ++seconds
             val average = iteration.get() / actSec
-            println("$iteration passwords tried after $actSec seconds. On average $average per second.")
+            var percentage = (iteration.toFloat() / (passwords.size).toFloat()).toDouble() * 100
+            if (percentage < 0.01) percentage = 0.01
+            println("$iteration/${passwords.size} (${percentage.toString().substring(0, 4)})% passwords tried after $actSec seconds. On average $average per second.")
             Thread.sleep(1000)
             if (finished) break
         }
         readLine()
+    }
+
+    private fun startSearchInRange(
+        subRange: IntRange,
+        passwords: ArrayList<String>
+    ) {
+        Completable.fromAction {
+            for (index in subRange) {
+                iteration.getAndIncrement()
+                if (Verifier.verify(path, passwords[index])) {
+                    finished = true
+                    println("Password to the zip archive at $path === ${passwords[index]}")
+                    Runtime.getRuntime().exit(0)
+                }
+            }
+        }.subscribeOn(Schedulers.computation())
+            .subscribe {}
+    }
+    private fun startSearchInRangeWithPrinting(
+        subRange: IntRange,
+        passwords: ArrayList<String>
+    ) {
+        Completable.fromAction {
+            for (index in subRange) {
+                iteration.getAndIncrement()
+                print(" ${passwords[index]} ")
+                if (Verifier.verify(path, passwords[index])) {
+                    finished = true
+                    println("Password to the zip archive at $path === ${passwords[index]}")
+                    Runtime.getRuntime().exit(0)
+                }
+            }
+        }.subscribeOn(Schedulers.computation())
+            .subscribe {}
     }
 
     private fun countIterations() {
@@ -74,7 +106,7 @@ class Bruteforcer(
         var resultList = ArrayList<String>()
         if (depth == 1) {
             characters.forEach { resultList.add(it) }
-            println("Created a list for $depth depth")
+            println("Created a list for $depth depth, size = ${resultList.size}")
             return resultList
         }
 
